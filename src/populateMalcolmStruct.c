0b928972e456287ea53f6bcdd33816ad4ef23eda
eprei <93380002+eprei@users.noreply.github.com>
2024-11-03 13:45:53 +0100

    style: change names to snake_case

--- src/populateMalcolmStruct.c
+++
@@ -1,8 +0,0 @@
-#include "../includes/ft_malcolm.h"
-
-void populateMalcolmStruct(char **argv, t_malcolm *malcolm_struct){
-  malcolm_struct->sourceIp = argv[1];
-  malcolm_struct->sourceMacAddress = argv[2];
-  malcolm_struct->targetIp = argv[3];
-  malcolm_struct->targetMacAddress = argv[4];
-}
