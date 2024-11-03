1b4449b6796b8246069ab282d6bbb7a214aad043
eprei <93380002+eprei@users.noreply.github.com>
2024-11-03 13:35:04 +0100

    feat: add centralized message handling

---
+++ includes/messages_to_print.h
@@ -0,0 +1,8 @@
+#ifndef MESSAGES_TO_PRINT_H
+# define MESSAGES_TO_PRINT_H
+
+# define USAGE_MSG "Usage: " BINARY_NAME " <source ip> <source mac address> <target ip> <target mac address>\n"
+# define INVALID_IP_MSG RED "Unknown host or invalid IP address: %s\n" RESET
+# define INVALID_MAC_MSG RED "Invalid MAC address: %s\n" RESET
+
+#endif //MESSAGES_TO_PRINT_H
